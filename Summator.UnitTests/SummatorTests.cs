9e1f98f5126a06676b6d6e742ae44b61bfbcf3d9
skipmin <83446768+skipmin@users.noreply.github.com>
2023-01-25 17:41:51 +0200

    asd

---
+++ Summator.UnitTests/SummatorTests.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+
+namespace Summator.UnitTests
+{
+    public class SummatorTests
+    {
+        [Test]
+        public void Test_Summator_SumTwoPositiveNumbers()
+        {
+            var nums = new int[] { 1, 2 };
+            var actual = Summator.Sum(nums);
+            var expected = 3;
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test_Summator_SumTwoNegativeNumbers()
+        {
+            var nums = new int[] { -1, -99 };
+            var actual = Summator.Sum(nums);
+            var expected = -100;
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test_Summator_OneNumber()
+        {
+            var nums = new int[] { 6 };
+            var actual = Summator.Sum(nums);
+            var expected = 6;
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
